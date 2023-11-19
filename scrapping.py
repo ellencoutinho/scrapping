21ac3e5e6dca96ffe6c916c1bddd414790dbf3f9
Ellen Coutinho <ellencls@al.insper.edu.br>
2023-11-18 22:59:57 -0300

    Pega informações da franquia hardcoded

--- scrapping.py
+++ scrapping.py
@@ -12,5 +12,45 @@ for e in filtro1:
             i+=1
             print(e.get('title'))
             print(e.get('href'))
+            # [1]
 
 print(i)
+
+# Teste para a franquia hardcoded que seria pego em [1]
+url_franquia = "https://franquias.portaldofranchising.com.br/franquia-kfc/"
+page_franquia = requests.get(url_franquia)
+soup = BeautifulSoup(page_franquia.text, 'html.parser')
+n_unidades = soup.find_all('strong')[2].get_text()
+contato = soup.find_all('strong')[3].get_text()
+
+achou_investimento = False
+achou_retorno = False
+achou_contato = False
+achou_faturamento = False
+achou_unidades = False
+
+faturamento = "Não informado"
+
+for e in soup.find_all('strong'):
+    conteudo = e.get_text()
+    separacao_string = conteudo.split()
+    print(e.get_text())
+    print(separacao_string)
+    if not achou_investimento and separacao_string[0] == 'a':
+        investimento = conteudo
+        achou_investimento = True
+        print('Sou investimento')
+    elif not achou_retorno and separacao_string[0] == 'de':
+        retorno = conteudo
+        achou_retorno = True
+        print('Sou retorno')
+    elif not achou_faturamento and separacao_string[0][0] == 'R':
+        faturamento = conteudo
+        achou_faturamento = True
+    elif not achou_contato and len(separacao_string)==2:
+        contato = conteudo
+        achou_contato = True
+    elif not achou_unidades:
+        n_unidades = conteudo
+        achou_unidades = True
+print(f'investimento: {investimento}, retorno {retorno}, faturamento {faturamento}, n_unidades {n_unidades}, contato {contato}')
